e7fda8e1220c76ea57d79a0a896948e5dcfcba61
Nander <nanderhokwerda@gmail.com>
2015-11-03 23:10:17 +0100

    Added prerequisite installer

---
+++ prerequisites.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get install git iftop iptraf vim curl wget lighttpd python2.7-dev swig python-setuptools build-essential openssl python-lxml libssl-dev libxml2-dev libxslt1-dev python-m2crypto -y
+
+sudo easy_install pip
+
+sudo pip install netaddr
+
+sudo pip install M2Crypto
+
+sudo pip install cherrypy
+
+sudo pip install mako
+
+sudo pip install dnspython
+
+sudo pip install metapdf
+
+git clone https://github.com/smicallef/spiderfoot.git
